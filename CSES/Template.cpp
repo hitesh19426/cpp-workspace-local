63aff28c4d814dd12915535da565607bab51388c
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2021-07-26 16:09:46 +0530

    Add files via upload

--- CSES/Template.cpp
+++ CSES/Template.cpp
@@ -3,41 +3,45 @@ using namespace std;
 void solve();
 
 #define endl '\n'
-#define mod 1000000007
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define nline '\n'
-#define INF INT_MAX
-#define mod1 998244353
-#define INFLL LLONG_MAX
-#define PI 3.141592653589793238462
-
 #define vi vector<int>
 #define vb vector<bool>
 #define pii pair<int, int>
 #define vll vector<long long>
 #define vvi vector<vector<int>>
-#define vvb vector<vector<bool>>
 #define vpii vector<pair<int, int>>
 #define pll pair<long long, long long>
 
+#define nline '\n'
+#define inf 1000000000
+#define mod 1000000007
+#define mod1 998244353
+#define inf_ll (ll)1e18
+#define PI 3.141592653589793238462
 #define ff first
 #define ss second
 #define pb push_back
 #define mp make_pair
 #define eb emplace_back
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+
 #define rep(i, n)	for (int i=0;i<n;i++)
 #define reps(i, a, n)	for (int i=a;i<n;i++)
 #define foreach(itr, v) for (auto itr=v.begin();itr!=v.end();itr++)
 
 #ifndef ONLINE_JUDGE
 #define print(x) cerr<< #x << " = "; _print(x); cerr<<endl;
+#define printarr(x, size) cerr<< #x << " = "; _print(x, size); cerr<<endl;
 #else
 #define print(x)
 #endif
+
+void _print(bool t) {cerr << t;}
 void _print(ll t) {cerr << t;}
 void _print(ld t) {cerr << t;}
 void _print(ull t) {cerr << t;}
@@ -45,9 +49,11 @@ void _print(int t) {cerr << t;}
 void _print(char t) {cerr << t;}
 void _print(double t) {cerr << t;}
 void _print(string t) {cerr << t;}
-template <class T, size_t N> void _print(T (&arr)[N]) {cerr << "[ "; for(T v:arr) { cerr<< (v) <<" "; } cerr<<"]";}
+template <class T> void _print(T arr[], int size) {cerr << "[ "; for(int i=0; i<size; i++) { cerr << arr[i] <<" "; } cerr<<"]";}
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(vector <vector<T>> v) {cerr << ""; for (vector<T> i : v) {cerr << '\n'; _print(i);}}
+template <class T> void _print(list <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
@@ -72,5 +78,5 @@ int main(int argc, char const *argv[])
 }
 
 void solve(){
-	
+	cout << 1 << endl;
 }
