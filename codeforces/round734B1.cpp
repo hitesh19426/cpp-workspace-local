6b61f422498f44ff9b9dc210f5df2cbe9ad86de1
Hitesh Garg <55842142+hitesh19426@users.noreply.github.com>
2021-07-26 16:05:27 +0530

    Add files via upload

---
+++ codeforces/round734B1.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve();
+
+#define endl '\n'
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define vi vector<int>
+#define vb vector<bool>
+#define pii pair<int, int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vpii vector<pair<int, int>>
+#define pll pair<long long, long long>
+
+#define endl '\n'
+#define nline '\n'
+#define inf 1000000000
+#define mod 1000000007
+#define mod1 998244353
+#define inf_ll (ll)1e18
+#define PI 3.141592653589793238462
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define set_bits __builtin_popcountll
+#define all(x) (x).begin(), (x).end()
+
+#define rep(i, n)	for (int i=0;i<n;i++)
+#define reps(i, a, n)	for (int i=a;i<n;i++)
+#define foreach(itr, v) for (auto itr=v.begin();itr!=v.end();itr++)
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+#define print(x) cerr<< #x << " = "; _print(x); cerr<<endl;
+#else
+#define print(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+template <class T, size_t N> void _print(T (&arr)[N]) {cerr << "[ "; for(T v:arr) { cerr<< (v) <<" "; } cerr<<"]";}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*----------------------------------------------------------------------------------------------------*/
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("errorf.in", "w", stderr);
+#endif
+
+	int t=1;
+	cin>>t;
+	while(t--){
+		solve();
+		cout<<endl;
+	}
+	return 0;
+}
+
+void solve(){
+	string s;
+	cin>>s;
+
+	int n = s.size();
+
+	int arr[26] = {0};
+	for(char c:s)
+		arr[c-'a']++;
+
+	for(int i=0; i<; i++){
+		if(arr[i]>2)
+			arr[i] = 2;
+	}
+
+	int count = 0, sum=0;
+	for(int i=0; i<26; i++){
+		sum += arr[i];
+	}
+	cout << sum/2;
+
+	// for(int i=0; i<26; i++){
+	// 	if(arr[i] == 2){
+	// 		count ++;
+	// 		arr[i] = 0;
+	// 	}
+	// }
+
+}
